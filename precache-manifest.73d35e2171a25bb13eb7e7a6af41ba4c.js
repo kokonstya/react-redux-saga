865b356d458bad4c2aaa4569b3a6b73291c561f0
kokonstya <kokonstya@gmail.com>
2020-03-23 01:53:13 +0700

    add files

---
+++ precache-manifest.73d35e2171a25bb13eb7e7a6af41ba4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027e2e5f177b4c9838959117bed851f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33a600de444bfc61bcec",
+    "url": "/static/js/2.74f0143f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.74f0143f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574d66f7d742c699e669",
+    "url": "/static/js/main.3673691e.chunk.js"
+  },
+  {
+    "revision": "bbb977733dc69633edc8",
+    "url": "/static/js/runtime-main.784c6230.js"
+  }
+]);
